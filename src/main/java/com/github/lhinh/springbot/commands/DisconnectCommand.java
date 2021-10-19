cfe63a0eb465f7848e3297202b3f3332a68d1cde
Loc Hinh <lhinh3431@gmail.com>
2021-10-19 13:53:20 -0700

    implemented voice disconnect

--- src/main/java/com/github/lhinh/springbot/commands/DisconnectCommand.java
+++ src/main/java/com/github/lhinh/springbot/commands/DisconnectCommand.java
@@ -1,13 +1,7 @@
 package com.github.lhinh.springbot.commands;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
 
-import discord4j.core.GatewayDiscordClient;
-import discord4j.core.object.entity.Guild;
-import discord4j.common.util.Snowflake;
-import discord4j.voice.VoiceConnection;
+import org.springframework.stereotype.Component;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
 import reactor.core.publisher.Mono;
 
@@ -21,7 +15,6 @@ public class DisconnectCommand implements SlashCommand {
 
 	@Override
 	public Mono<Void> handle(ChatInputInteractionEvent event) {
-//		final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 		
 //		GatewayDiscordClient client = event.getClient();
 //		Snowflake guildId = event.getInteraction().getGuildId().get();
@@ -29,9 +22,9 @@ public class DisconnectCommand implements SlashCommand {
 //		Mono<Guild> voiceMono = Mono.justOrEmpty(event.getInteraction().getGuild())
 //				.flatMap(Guild::getVoiceConnection);
 
-//		Mono<Object> voiceMono = Mono.justOrEmpty(event.getClient())
-//				.flatMap(gatewayDiscordClient -> Mono.justOrEmpty(event.getInteraction().getGuildId())
-//						.flatMap(guildId -> gatewayDiscordClient.getVoiceConnectionRegistry().disconnect(guildId)));
+		Mono<Object> voiceMono = Mono.justOrEmpty(event.getClient())
+				.flatMap(gatewayDiscordClient -> Mono.justOrEmpty(event.getInteraction().getGuildId())
+						.flatMap(guildId -> gatewayDiscordClient.getVoiceConnectionRegistry().disconnect(guildId)));
 //		Mono<Object> voiceMono = Mono.justOrEmpty(event.getInteraction().getGuildId())
 //				.flatMap(guildId -> Mono.justOrEmpty(event.getInteraction().getClient())
 //						.flatMap(client -> {
@@ -45,10 +38,10 @@ public class DisconnectCommand implements SlashCommand {
 //				.flatMap(VoiceChannel::getVoiceConnection)
 //				.flatMap(connection -> connection.disconnect());
 
-		return event.reply()
+		return event.reply("Disconnected!")
 				.withEphemeral(true)
-				.withContent("Disconnected!");
-//				.and(test);
+//				.withContent("Disconnected!");
+				.and(voiceMono.then());
 	}
 
 }
