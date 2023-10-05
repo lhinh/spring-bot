a92e7ebf456b561dfaac0759b0714acc5645def5
Loc Hinh <lhinh3431@gmail.com>
2023-10-05 08:33:47 -0700

    using event listener interface and registering
    ...listeners through gateway discord client builder
    WIP auto disconnect listener; voice state counter not computed properly

--- src/main/java/com/github/lhinh/springbot/config/DiscordConfig.java
+++ src/main/java/com/github/lhinh/springbot/config/DiscordConfig.java
@@ -1,12 +1,18 @@
 package com.github.lhinh.springbot.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.github.lhinh.springbot.listeners.EventListener;
+
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
 import discord4j.rest.RestClient;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
 
 @RequiredArgsConstructor
 @Configuration
@@ -15,11 +21,25 @@ public class DiscordConfig {
     private final DiscordConfigProperties discordConfigProperties;
 
     @Bean
-    public GatewayDiscordClient gatewayDiscordClient() {
-        return DiscordClientBuilder.create(discordConfigProperties.getBotToken()).build()
+    public <T extends Event> GatewayDiscordClient gatewayDiscordClient(List<EventListener<T>> eventListener) {
+        GatewayDiscordClient discordClient = DiscordClientBuilder.create(discordConfigProperties.getBotToken()).build()
             .gateway()
+            .withEventDispatcher(dispatcher -> {
+                return Flux.fromIterable(eventListener)
+                    .flatMap(listener -> {
+                        return dispatcher.on(listener.getEventType()).flatMap(listener::handle).onErrorResume(listener::handleError);
+                    });
+            })
             .login()
             .block();
+
+        // for (EventListener<T> listener : eventListener) {
+        //     discordClient.on(listener.getEventType(), listener::handle)
+        //         .onErrorResume(listener::handleError)
+        //         .subscribe();
+        // }
+
+        return discordClient;   
     }
     
     @Bean
