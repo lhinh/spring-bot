73dfb0a5a3d1e4a642a077f6ccfd7bf965c5fb78
Loc Hinh <35240509+lhinh@users.noreply.github.com>
2023-10-06 15:53:52 -0700

    Enhancement/listener support (#8)
    * using event listener interface and registering
    
    ...listeners through gateway discord client builder
    WIP auto disconnect listener; voice state counter not computed properly
    
    * putting on ice until D4J devs can troubleshoot
    
    ...VoiceStatusUpdate event. member count is not calculated properly

---
+++ src/main/java/com/github/lhinh/springbot/listeners/EventListener.java
@@ -0,0 +1,18 @@
+package com.github.lhinh.springbot.listeners;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import discord4j.core.event.domain.Event;
+import reactor.core.publisher.Mono;
+
+public interface EventListener<T extends Event> {
+    Logger log = LoggerFactory.getLogger(EventListener.class);
+
+    Class<T> getEventType();
+    Mono<Void> handle(T event);
+
+    default Mono<Void> handleError(Throwable error) {
+        log.error("Unable to process event" + getEventType().getSimpleName(), error);
+        return Mono.empty();
+    }
+}
