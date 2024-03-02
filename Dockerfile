6d6283cc6fb5ec05bb19299a4f058353ed7732ce
Loc Hinh <35240509+lhinh@users.noreply.github.com>
2024-03-02 12:47:57 -0800

    Youtube search retrieves first result (#14)
    * updated lavaplayer to maintained version
    
    * using search queries now retrieves the first re...
    
    ...sult and loads without clearing playlist
    updated dockerfile and build version

--- Dockerfile
+++ Dockerfile
@@ -5,10 +5,11 @@ FROM openjdk:21-jdk
 WORKDIR /app
 
 # Copy the packaged jar file into the container at /app
-COPY target/spring-bot-v0.1.0.jar /app
+COPY target/spring-bot-v0.1.1.jar /app
 
 # Make port 8080 available to the world outside this container
 # EXPOSE 8080
 
 # Run the jar file
-CMD ["java", "-jar", "spring-bot-v0.1.0.jar"]
+CMD ["java", "-jar", "spring-bot-v0.1.1.jar"]
+VOLUME /app/logs
--- src/main/java/com/github/lhinh/springbot/musicplayer/GuildAudioManager.java
+++ src/main/java/com/github/lhinh/springbot/musicplayer/GuildAudioManager.java
@@ -9,6 +9,11 @@ import org.springframework.stereotype.Component;
 
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.source.AudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAudioSourceManager;
+import com.sedmelluq.discord.lavaplayer.track.AudioItem;
+import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
+import com.sedmelluq.discord.lavaplayer.track.AudioReference;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 
 import discord4j.common.util.Snowflake;
@@ -25,6 +30,7 @@ public class GuildAudioManager{
     private final LavaPlayerAudioProvider provider;
     private final AudioPlayerManager audioPlayerManager;
     private final Map<Snowflake, GuildAudioManager> guildAudioManagers = new ConcurrentHashMap<>();
+    private String lastSearchedLink = "";
     
 
     public GuildAudioManager of(Snowflake guildId) {
@@ -47,7 +53,27 @@ public class GuildAudioManager{
      */
     public Void loadItem(String link) {
         try {
-            audioPlayerManager.loadItem(link, new AudioTrackLoadResultHandler(scheduler)).get();
+            if (link.startsWith("ytsearch:")) {
+                AudioSourceManager ytAudioSourceManager = audioPlayerManager.getSourceManagers().stream()
+                        .filter(source -> source instanceof YoutubeAudioSourceManager)
+                        .findFirst()
+                        .orElseThrow();
+
+                AudioItem item = ytAudioSourceManager.loadItem(audioPlayerManager, new AudioReference(link, null));
+
+                if (item instanceof AudioPlaylist) {
+                    AudioTrack firstResult = ((AudioPlaylist)item).getTracks().stream()
+                    .findFirst()
+                    .orElseThrow();
+
+                    lastSearchedLink = firstResult.getInfo().uri;
+                    scheduler.play(firstResult);
+                } else {
+                    scheduler.play((AudioTrack)item);
+                }
+            } else {
+                audioPlayerManager.loadItem(link, new AudioTrackLoadResultHandler(scheduler)).get();
+            }
         } catch (InterruptedException | ExecutionException e) {
             log.error(e.getMessage());
         }
