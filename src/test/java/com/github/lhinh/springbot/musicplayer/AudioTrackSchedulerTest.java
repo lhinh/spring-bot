901e8926d005dc969439ae7e4f16bfe62bfb3049
Loc Hinh <35240509+lhinh@users.noreply.github.com>
2024-03-03 15:26:41 -0800

    test circle config (#20)
    * updated workflow name
    
    * Youtube search retrieves first result (#14)
    
    * updated lavaplayer to maintained version
    
    * using search queries now retrieves the first re...
    
    ...sult and loads without clearing playlist
    updated dockerfile and build version
    
    * Add tests with coverage (#15)
    
    * added code coverage and starter test
    
    * updated circle config
    
    circle will build and test on any branch changes. deploy to docker
    happens only on merge to main
    
    * fixed typo
    
    * fixed another typo
    
    * Add .circleci/config.yml
    
    * Revert "Add .circleci/config.yml"
    
    * explicitly naming dockerfile to run
    
    * adding '-checkout' to docker-push job
    
    * added extra steps for circle config
    
    * added missing step
    
    * updated config
    
    * docker image tags are used from git on tag push (#19)
    
    circle also will tag minor builds on merges to main

---
+++ src/test/java/com/github/lhinh/springbot/musicplayer/AudioTrackSchedulerTest.java
@@ -0,0 +1,113 @@
+package com.github.lhinh.springbot.musicplayer;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AudioTrackSchedulerTest {
+        
+    @Test
+    void testAudioTrackSchedulerPlay_EmptyPlaylist() {
+        boolean force = true;
+        AudioPlayer player = Mockito.mock(AudioPlayer.class);
+        AudioTrackScheduler scheduler = new AudioTrackScheduler(player);
+        
+        AudioTrack track = Mockito.mock(AudioTrack.class);
+        Mockito.when(player.startTrack(track, force)).thenReturn(true);
+        boolean isPlayingThisTrack = scheduler.play(track);
+        boolean isCurrentlyPlaying = scheduler.isCurrentlyPlaying();
+        
+        assertTrue(isPlayingThisTrack);
+        assertTrue(isCurrentlyPlaying);
+    }
+
+    @Test
+    void testAudioTrackSchedulerPlay_AddOneToPlaylist() {
+        boolean force = true;
+        AudioPlayer player = Mockito.mock(AudioPlayer.class);
+        AudioTrackScheduler scheduler = new AudioTrackScheduler(player);
+        
+            
+        AudioTrack track1 = Mockito.mock(AudioTrack.class);
+        AudioTrack track2 = Mockito.mock(AudioTrack.class);
+
+        Mockito.when(player.startTrack(track1, force)).thenReturn(true);
+
+        boolean isPlayingThisTrack = scheduler.play(track1);
+        boolean isCurrentlyPlaying = scheduler.isCurrentlyPlaying();
+        assertTrue(isPlayingThisTrack);
+        assertTrue(isCurrentlyPlaying);
+
+        isPlayingThisTrack = scheduler.play(track2);
+        isCurrentlyPlaying = scheduler.isCurrentlyPlaying();
+        assertFalse(isPlayingThisTrack);
+        assertTrue(isCurrentlyPlaying);
+    }
+
+    @Test
+    void testAudioTrackSchedulerPlay_AddPlaylistToPlaylist() {
+        //TODO
+    }
+
+    @Test
+    void testAudioTrackSchedulerPlay_YTSearchQueryEmptyPlaylist() {
+        //TODO
+    }
+
+    @Test
+    void testAudioTrackSchedulerPlay_YTSearchQueryPlaylistLoaded() {
+        //TODO
+    }
+    
+    @Test
+    void testAudioTrackSchedulerSkip_SkipToStop() {
+        boolean force = true;
+        AudioPlayer player = Mockito.mock(AudioPlayer.class);
+        AudioTrackScheduler scheduler = new AudioTrackScheduler(player);
+        
+            
+        AudioTrack track1 = Mockito.mock(AudioTrack.class);
+        AudioTrack track2 = Mockito.mock(AudioTrack.class);
+
+        Mockito.when(player.startTrack(track1, force)).thenReturn(true);
+
+        scheduler.play(track1);
+        boolean isCurrentlyPlaying = scheduler.isCurrentlyPlaying();
+        assertTrue(isCurrentlyPlaying);
+
+        scheduler.play(track2);
+        isCurrentlyPlaying = scheduler.isCurrentlyPlaying();
+        assertTrue(isCurrentlyPlaying);
+        
+        Mockito.when(player.startTrack(Mockito.any(AudioTrack.class), Mockito.eq(!force))).thenReturn(true);
+
+        boolean skipResult = scheduler.skip();
+        isCurrentlyPlaying = scheduler.isCurrentlyPlaying();
+        assertTrue(skipResult);
+        assertTrue(isCurrentlyPlaying);
+
+        skipResult = scheduler.skip();
+        isCurrentlyPlaying = scheduler.isCurrentlyPlaying();
+        assertFalse(skipResult);
+        assertFalse(isCurrentlyPlaying);
+    }
+    
+    @Test
+    void testAudioTrackSchedulerStop() {
+        boolean force = true;
+        AudioPlayer player = Mockito.mock(AudioPlayer.class);
+        AudioTrackScheduler scheduler = new AudioTrackScheduler(player);
+        
+        AudioTrack track = Mockito.mock(AudioTrack.class);
+        Mockito.when(player.startTrack(track, force)).thenReturn(true);
+        scheduler.play(track);
+        
+        scheduler.stop();
+        assertTrue(scheduler.getQueue().size() == 0);
+        assertFalse(scheduler.isCurrentlyPlaying());
+    }
+    
+}
