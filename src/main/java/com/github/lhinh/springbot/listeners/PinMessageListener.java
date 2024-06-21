45b1478a0079115ec80601ef54de72ab96c9cf5c
Loc Hinh <lhinh3431@gmail.com>
2024-06-20 21:30:20 -0700

    added message ID check to avoid dupes*
    added logic to embed image links properly
    *dupes still possible due to message IDs being eventually updated
    on discord side

--- src/main/java/com/github/lhinh/springbot/listeners/PinMessageListener.java
+++ src/main/java/com/github/lhinh/springbot/listeners/PinMessageListener.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import org.springframework.stereotype.Component;
 
 import com.github.lhinh.springbot.config.DiscordConfigProperties;
+import com.github.lhinh.springbot.util.HttpLinkUtil;
 
 import discord4j.core.event.domain.message.ReactionAddEvent;
 import discord4j.core.object.entity.Attachment;
@@ -18,18 +19,16 @@ import discord4j.core.object.entity.channel.Channel;
 import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.core.spec.TextChannelCreateSpec;
 import discord4j.rest.util.Color;
+import lombok.RequiredArgsConstructor;
 import reactor.core.publisher.Mono;
 
+@RequiredArgsConstructor
 @Component
 public class PinMessageListener implements EventListener<ReactionAddEvent>{
 
     private final DiscordConfigProperties discordConfigProperties;
     // For posterity
-    private final Color nutMeat = Color.of(222, 197, 183);
-
-    public PinMessageListener(DiscordConfigProperties discordConfigProperties) {
-        this.discordConfigProperties = discordConfigProperties;
-    }
+    // private final Color nutMeat = Color.of(222, 197, 183);
 
     @Override
     public Class<ReactionAddEvent> getEventType() {
@@ -59,16 +58,22 @@ public class PinMessageListener implements EventListener<ReactionAddEvent>{
         String messageHyperlink = String.format("[%s](%s)", "Jump!", messageUrl);
         EmbedCreateSpec.Builder pinContentEmbed = EmbedCreateSpec.builder()
             .author(message.getAuthor().orElseThrow().getUsername(), null, message.getAuthor().orElseThrow().getAvatarUrl())
-            .description(message.getContent())
             .color(Color.TAHITI_GOLD)
             .footer(message.getId().asString(), null)
             .timestamp(Instant.now())
             .addField("**Source**", messageHyperlink, false);
     
-        List<String> imageUrls = extractUrls(message.getAttachments(), true);
+        if (HttpLinkUtil.isImage(message.getContent())) {
+
+            pinContentEmbed.image(message.getContent());
+        } else {
+            pinContentEmbed.description(message.getContent());
+
+            List<String> imageUrls = extractUrls(message.getAttachments(), true);
+            imageUrls.stream().findFirst().ifPresent(pinContentEmbed::image);
+        }
+
         List<String> attachmentUrls = extractUrls(message.getAttachments(), false);
-    
-        imageUrls.stream().findFirst().ifPresent(pinContentEmbed::image);
         if (!attachmentUrls.isEmpty()) {
             String attachmentsField = String.join("\n", attachmentUrls);
             pinContentEmbed.addField("Attachments", attachmentsField, false);
@@ -107,7 +112,7 @@ public class PinMessageListener implements EventListener<ReactionAddEvent>{
             .flatMapMany(Guild::getChannels)
             .filter(channel -> channel.getName().equals(channelName) && channel.getType() == Channel.Type.GUILD_TEXT)
             .cast(TextChannel.class)
-            .flatMap(channel -> channel.getMessagesBefore(message.getId()))
+            .flatMap(channel -> channel.getMessagesBefore(channel.getLastMessageId().orElseThrow()))
             .flatMapIterable(msg -> msg.getEmbeds())
             .any(embed -> embed.getFooter().map(footer -> footer.getText().equals(message.getId().asString())).orElse(false));
     }
